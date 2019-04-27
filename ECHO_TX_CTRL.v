ef179b30f2eb7452390bd0bcf68606735520075d
liman jihe <rixinshi@foxmail.com>
2019-04-28 01:53:27 +0800

    Create ECHO_TX_CTRL.v

---
+++ ECHO_TX_CTRL.v
@@ -0,0 +1,13 @@
+'timescale 1ns/1ps
+
+module echo_tx_ctrl (
+    input wire  sys_clk    ,
+    input wire  sys_rst    ,
+    input wire [63:0]  echo_tx_data   ,
+    input wire         echo_tx_en     ,
+    output wire [63:0]  srio_tx_data    ,
+    output wire         srio_tx_en    
+);
+
+
+endmoudle
